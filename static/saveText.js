f2dd138a942ad150e017abf7ab96aca7a0d261c3
zhiqiang.xu <zhiqiang.xu@163.com>
2022-03-20 09:36:06 +0800

    222

--- static/saveText.js
+++ static/saveText.js
@@ -1,60 +1,45 @@
-function doSave(value, type, name){
- 
-  let blob;
- 
-  if (typeof window.Blob == "function") {
- 
-    blob = new Blob([value], {
- 
-      type: type
- 
-    });
- 
-  } else {
- 
+
+
+
+let value="hello world 123";
+let type="txt";
+let name = "hello.txt";
+let blob;
+
+if (typeof window.Blob == "function") {
+    blob = new Blob([value], {type: type});
+} else {
     let BlobBuilder = window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder;
- 
     let bb = new BlobBuilder();
- 
     bb.append(value);
- 
     blob = bb.getBlob(type);
- 
-  }
- 
-  let URL = window.URL || window.webkitURL;
- 
-  let bloburl = URL.createObjectURL(blob);
- 
-  let anchor = document.createElement("a");
- 
-  if ('download' in anchor) {
- 
-    anchor.style.visibility = "hidden";
- 
-    anchor.href = bloburl;
- 
-    anchor.download = name;
- 
-    document.body.appendChild(anchor);
- 
-    let evt = document.createEvent("MouseEvents");
- 
-    evt.initEvent("click", true, true);
- 
-    anchor.dispatchEvent(evt);
- 
-    document.body.removeChild(anchor);
- 
-  } else if (navigator.msSaveBlob) {
- 
-    navigator.msSaveBlob(blob, name);
- 
-  } else {
- 
-    location.href = bloburl;
- 
-  }
- 
 }
- 
+
+let URL = window.URL || window.webkitURL;
+
+let bloburl = URL.createObjectURL(blob);
+
+let anchor = document.createElement("a");
+
+if ('download' in anchor) {
+
+    anchor.style.visibility = "hidden";
+
+    anchor.href = bloburl;
+
+    anchor.download = name;
+
+    document.body.appendChild(anchor);
+
+    let evt = document.createEvent("MouseEvents");
+
+    evt.initEvent("click", true, true);
+
+    anchor.dispatchEvent(evt);
+
+    document.body.removeChild(anchor);
+} else if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, name);
+} else {
+    location.href = bloburl;
+}
